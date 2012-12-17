5a2f3a5ad1baac758b6b61bf23348305631cc465
Tres Seaver <tseaver@palladion.com>
2012-12-17 20:28:50 +0000

    Module header.

--- transaction/tests/test_attempt.py
+++ transaction/tests/test_attempt.py
@@ -1,6 +1,21 @@
+##############################################################################
+#
+# Copyright (c) 2012 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
 import unittest
 
+
 class TestAttempt(unittest.TestCase):
+
     def _makeOne(self, manager):
         from transaction._manager import Attempt
         return Attempt(manager)
