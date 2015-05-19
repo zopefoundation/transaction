52bcf5b38bee2972b75c520c5f4afcc7a53ee6ce
Tres Seaver <tseaver@palladion.com>
2015-05-19 17:13:17 -0400

    svb

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def _read_file(filename):
 README = _read_file('README.rst') + '\n\n' + _read_file('CHANGES.rst')
 
 setup(name='transaction',
-      version='1.4.4',
+      version='1.4.5.dev0',
       description='Transaction management for Python',
       long_description=README,
       classifiers=[
